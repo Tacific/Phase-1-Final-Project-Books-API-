e0716626657c645a40b58403b4f0e0ffb619b219
Chantha Voeun <cvoeun45@gmail.com>
2023-05-12 14:46:25 -0400

    creat fetch

--- script.js
+++ script.js
@@ -1,32 +1,8 @@
-//1st Part Create DOM and Fetch
-
-document.addEventListener("DOMContentLoaded", () => 
-    fetch ("http://localhost:3000/book")
-    .then((response) => response.json())
-    .then((books) => {
-    books.forEach(bookReader)}))
-
-//Build eventListiners
-function displayImage(src, width, height) {
-    var img = document.createElement("img");
-    img.src = src;
-    img.width = width;
-    img.height = height;
-    document.body.appendChild(img);
-   }
-
-const booksToRead = document.querySelector('.books-to-read')
-console.log(booksToRead)
-
-function bookReader (bookData) {
-    const bookDiv = document.createElement("div")
-    const bookPara = document.createElement("p")
-    bookPara.textContent = bookData.img
-    bookDiv.append(bookPara)
-    booksToRead.append(bookDiv)
-}
-
-function formMaker(){
-
-
-}
+//Assign fetch to variable
+const fetchAPI = () => {
+    fetch("http://localhost:3000/mountains")
+    .then(response => response.json())
+    .then(places => addPlaces(places))
+    }
+    //Have to call function
+    fetchAPI()
